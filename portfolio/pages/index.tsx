bdc9ac2ae5839e3597b9d3b25e526cf0dcaa3247
[zmemer] <abdulaaqib4@gmail.com>
2024-02-28 17:28:33 +0000

    v22

---
+++ portfolio/pages/index.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header'
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Abdul`s Portfolio</title>
+      </Head>
+
+      <Header />
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills*/}
+      
+      {/* Projects */}
+
+      {/* Contact Me*/}
+
+    </div>
+  )
+}
+
+export default Home
