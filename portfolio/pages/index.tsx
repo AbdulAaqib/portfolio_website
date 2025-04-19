7f3a1e69033ac49810bc55c109bed31cec7f6860
AbdulAaqib <abdulaaqib4@gmail.com>
2025-04-19 01:18:38 +0100

    v12

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,5 +1,4 @@
 import type { GetStaticProps } from 'next';
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 import Head from 'next/head';
 
 import Link from 'next/link';
