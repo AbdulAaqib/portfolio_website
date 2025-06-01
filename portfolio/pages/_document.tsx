95f6abd60d9292658c4ccd62daad292ec172b753
AbdulAaqib <abdulaaqib4@gmail.com>
2025-06-01 18:57:06 +0100

    v20 website icon update

---
+++ portfolio/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/blue_ball_pic-removebg-preview.png" />
+        <link rel="apple-touch-icon" href="/blue_ball_pic-removebg-preview.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+} 
